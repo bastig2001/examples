9545c4f9777f5b17d7571283f339dd417c731db7
Sebastian Grman <sebastian.grman@gmail.com>
2021-07-19 20:06:02 +0200

    finish example

--- two_mutually_dependent_objects/cpp/example.cpp
+++ two_mutually_dependent_objects/cpp/example.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 
 #include "channel.h"
 #include "message.h"
@@ -9,15 +10,31 @@ using namespace std;
 
 
 int main() {
-    Channel<Message> a_to_b;
-    Channel<Message> b_to_a;
+    Channel<Message> to_b;
+    Channel<Message> to_a;
 
-    Object a("A", 42, b_to_a, a_to_b);
-    Object b("B", 64, a_to_b, b_to_a);
+    Object a("A", 42, to_a, to_b);
+    Object b("B", 64, to_b, to_a);
 
     thread thread_a{a};
     thread thread_b{b};
 
+    Message print_repeated{MsgType::Execute, Object::print_repeated("==", 8)};
+    Message get_value{MsgType::GetValue};
+
+    vector<Message> messages{
+        Message{MsgType::PrintHello},
+        Message{MsgType::PrintMessage, "Hello, World!"},
+        Message{MsgType::GetValue},
+        Message{MsgType::Value, 3},
+        Message{MsgType::Execute, Object::print_repeated(".", 10)},
+        Message{MsgType::SendToOther, &print_repeated},
+        Message{MsgType::SendToOther, &get_value}
+    };
+
+    to_a.send(messages);
+    to_b.send(messages);
+    
     thread_a.join();
     thread_b.join();
 }
